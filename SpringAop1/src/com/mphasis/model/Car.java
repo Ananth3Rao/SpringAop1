63939047e47d1a246d3502bde82ef9664c143e29
Ananth3Rao <ananth3rao24@gmail.com>
2021-09-01 14:33:59 -0400

    Adding Car Class

---
+++ SpringAop1/src/com/mphasis/model/Car.java
@@ -0,0 +1,25 @@
+package com.mphasis.model;
+
+public class Car {
+
+	String engine;
+	double weight;
+	public Car(String engine, double weight) {
+		super();
+		this.engine = engine;
+		this.weight = weight;
+	}
+	public String getEngine() {
+		return engine;
+	}
+	public void setEngine(String engine) {
+		this.engine = engine;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+}
